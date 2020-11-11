f0b228a44ed64cbc89828fc34c9d38b953f5aef5
Aritz Zugazaga <49875014+aritzugazaga@users.noreply.github.com>
2020-11-11 09:41:38 +0100

    Initial commit Forum

---
+++ apps/Forum/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .models import User
+from django.urls import reverse
+
+class Comment(models.Model):
+    userName = models.CharField(max_length=255, default = User.get_full_name())
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('forum_comment', args=[self.pk])
