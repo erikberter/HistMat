5cc8dd47faa8ff11eefff010b6edd899a20e57bb
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-11-16 10:14:40 +0100

    Avances en el foro

--- apps/Forum/models.py
+++ apps/Forum/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from .models import User
 from django.urls import reverse
 
-class post(models.Model):
+class Post(models.Model):
     userName = models.CharField(max_length=255, default = User.get_full_name())
     body = models.TextField()
+    image = models.ImageField(upload_to='model/img/users/', blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.name
+        return self.userName
 
     def get_absolute_url(self):
-        return reverse('forum_comment', args=[self.pk])
+        return reverse('forum_post', args=[self.pk])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="forum_comments", on_delete=models.CASCADE)
+    userName = models.CharField(max_length=255, default = User.get_full_name())
+    body = models.TextField()
+    likes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.userName
--- apps/Forum/views.py
+++ apps/Forum/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Post, Comment
 
-# Create your views here.
+def post_home(request):
+    posts = Post.objects.all()
+    context = {'posts' : posts}
+    return render(request, 'post.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, "post_detail.html", {'post':post})
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    success_url = reverse_lazy('post')
+
+class AddCommentView(CreateView):
+    model = Comment
+    template_name = 'add_post.html'
+    fields = '__all__'
+    success_url = reverse_lazy('post')
