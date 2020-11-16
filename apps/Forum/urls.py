8a1e166e530a7414319580a98c540b164d5e0db8
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-11-16 18:50:12 +0100

    Dearrollo en el foro

--- apps/Forum/urls.py
+++ apps/Forum/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 from django.conf import settings
+from .views import post_home, post_detail, AddPostView, AddCommentView
 
 from django.contrib.auth import views as auth_views
 
 app_name = 'forum'
 
 urlpatterns = [
-    path('forum/', auth_views.LogoutView.as_view() , name='logout'),
+    path('forum/', post_home , name='post_home'),
+    path('post_detail/add_post', AddPostView.as_view(), name = 'add_post'),
+    path('forum/post_detail/<int:pk>/', post_detail, name = 'post_detail'),
 ]
--- apps/Forum/views.py
+++ apps/Forum/views.py
@@ -6,7 +6,7 @@ from .models import Post, Comment
 def post_home(request):
     posts = Post.objects.all()
     context = {'posts' : posts}
-    return render(request, 'post.html', context)
+    return render(request, 'post_home.html', context)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -17,10 +17,10 @@ class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
-    success_url = reverse_lazy('post')
+    success_url = reverse_lazy('post_home')
 
 class AddCommentView(CreateView):
     model = Comment
     template_name = 'add_post.html'
     fields = '__all__'
-    success_url = reverse_lazy('post')
+    success_url = reverse_lazy('post_home')
