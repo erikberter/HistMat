e9f920f2e154975e9476f2499eb58a0ffac74a1a
ikerove <52752754+ikerove@users.noreply.github.com>
2020-11-25 09:43:19 +0100

    Añadir funcionalidad de apuntes

--- apps/Apuntes/views.py
+++ apps/Apuntes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Apunte
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ApunteCreateForm
 # Create your views here.
 
 def apuntes(request):
@@ -10,4 +13,20 @@ def apuntes(request):
 
 def apuntes_detail(request, pk):
     apunte = get_object_or_404(Apunte, pk = pk)
-    return render(request, 'apuntes_detail.html', {'articulo':articulo})
+    return render(request, 'Apuntes/apuntes_detail.html', {'apunte':apunte})
+
+
+class ApunteCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'Apuntes/forms/add_apuntes.html'
+    form_class = ApunteCreateForm
+
+    def form_valid(self, form):
+        apunte = apunte.save()
+        apunte.autor = self.request.user
+        
+        return HttpResponseRedirect(apunte.get_absolute_url())
+
+    def get_form_kwargs(self, *args, **kwargs):
+        kwargs = super(ApunteCreateView, self).get_form_kwargs(*args, **kwargs)
+        kwargs['user'] = self.request.user
+        return kwargs
