479577f5704247069ca368fc2be02d6eb5ef215f
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-06-08 21:45:41 +0200

    Mas pruebas

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = [ '*', 'histmat.herokuapp.com', 'localhost','127.0.0.1']
 
 
