1d7771ba176f7b47af37b44acee6ae4af330322e
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-06-08 19:12:29 +0200

    Procfile added

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = ['erikberter.pythonanywhere.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = [ 'histmat.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
