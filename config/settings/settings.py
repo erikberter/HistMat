f239809ed45379e1d5ff7a2c683843199452240d
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-06-08 21:03:08 +0200

    Update settings.py

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
 
-ALLOWED_HOSTS = [ 'histmat.herokuapp.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = [ '*', 'histmat.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
-if os.environ.get('GITHUB_WORKFLOW'):
+if os.environ.get('GITHUB_WORKFLOW', False):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
